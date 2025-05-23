e5013fde7b63de8e502cf8994ca3c8485fc059be
JessCardenas <jesscardenasbcn@gmail.com>
2025-05-23 12:28:26 +0200

    añadida lógica de pantalla oráculo y respuesta

--- src/components/OraculoScreen.jsx
+++ src/components/OraculoScreen.jsx
@@ -1,2 +1,34 @@
-function OraculoScreen() {}
+import { useState } from "react";
+import { obtenerRespuesta } from "../utils/respuestas";
+
+function OraculoScreen({ setPregunta, setRespuesta, cambiarPantalla }) {
+  const [input, setInput] = useState("");
+
+  const manejarPregunta = () => {
+    if (input.trim() === "") return;
+    setPregunta(input);
+    const respuesta = obtenerRespuesta();
+    setRespuesta(respuesta);
+    cambiarPantalla("resultado");
+  };
+
+  return (
+    <div className="text-center space-y-4">
+      <h2 className="text-2xl font-semibold">Haz tu pregunta</h2>
+      <input
+        type="text"
+        placeholder="¿Qué deseas saber?"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        className="p-2 rounded-md text-black"
+      />
+      <button
+        onClick={manejarPregunta}
+        className="block mx-auto mt-4 px-6 py-2 bg-indigo-600 hover:bg-indigo-700"
+      >
+        Preguntar
+      </button>
+    </div>
+  );
+}
 export default OraculoScreen;
--- src/components/RespuestaScreen.jsx
+++ src/components/RespuestaScreen.jsx
@@ -1,3 +1,20 @@
-function RespuestaScreen() {}
+function RespuestaScreen({ pregunta, respuesta, volver }) {
+  return (
+    <div className="text-center space-y-6">
+      <h2 className="text-2xl font-bold"> Tu pregunta: </h2>
+      <p className="italic">{pregunta}</p>
+      <div className="text-3xl font-bold text-green-400 animale-pulse">
+        {" "}
+        {respuesta}
+      </div>
+      <button
+        onClick={() => volver("home")}
+        className="mt-6 px-6 py-2 bg-gray-700 hoover:bg-gray-800 rounded-lg"
+      >
+        Volver al inicio
+      </button>
+    </div>
+  );
+}
 
 export default RespuestaScreen;

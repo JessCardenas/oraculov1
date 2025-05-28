0c1dd3fdc6d0cba70e0b226f05a6a9d1b82e565f
JessCardenas <jesscardenasbcn@gmail.com>
2025-05-28 09:57:24 +0200

    css update

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -1,9 +1,10 @@
-import Bola_manos from "../assets/images/Bola_manos.png";
+import Bola_manos from "../assets/images/Bola_manos.svg";
+import Estrella_vector from "../assets/images/Estrella_vector.png";
 
 function HomeScreen({ cambiarPantalla }) {
   return (
     <div className="text-center space-y-6">
-      <h2 className="text-8xl font-bold font-starlight">
+      <h2 className="text-8xl font-bold">
         ¿Dejamos que el destino decida por ti?
       </h2>
       <h1 className="text-4xl">
@@ -11,12 +12,35 @@ function HomeScreen({ cambiarPantalla }) {
         vez".
       </h1>
 
-      <button
-        onClick={() => cambiarPantalla("oraculo")}
-        className="mt-4 px-6 py-3   rounded-lg"
-      >
-        <img src={Bola_manos} alt="Pregunta al Oráculo" />
-      </button>
+      <div className="flex items-center justify-around">
+        <div>
+          <button
+            onClick={() => cambiarPantalla("oraculo")}
+            className="group relative flex items-center justify-center"
+          >
+            <img
+              src={Estrella_vector}
+              alt="Estrella"
+              className="absolute inset-0 w-full h-full opacity-0 group-hover:opacity-100 transition-opacity duration-300"
+            />
+
+            <p className="z-10">Sí</p>
+          </button>
+        </div>
+
+        <div>
+          <img src={Bola_manos} alt="Pregunta al Oráculo" />{" "}
+        </div>
+        <div>
+          <button
+            onClick={() => cambiarPantalla("oraculo")}
+            className="mt-4 px-6 py-3   rounded-lg"
+          >
+            <p>No</p>
+            <img src={Estrella_vector} className="estrella" />
+          </button>
+        </div>
+      </div>
     </div>
   );
 }

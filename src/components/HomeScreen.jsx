a8ac669d859450a1d36561353262419b3610aca2
JessCardenas <jesscardenasbcn@gmail.com>
2025-06-10 16:27:20 +0200

    update homescreen

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -4,7 +4,7 @@ import "../StyleSheets/HomeScreen.css";
 
 function HomeScreen({ cambiarPantalla }) {
   return (
-    <div className="text-center">
+    <div className="text-center container mx-auto">
       <h1 className="inicio max-w-xl mx-auto">
         ¿Dejamos que el destino decida por ti?
       </h1>

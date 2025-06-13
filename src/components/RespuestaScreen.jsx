9d5448e9c3888dc5e240a8a62ccc2ce54982a50e
JessCardenas <jesscardenasbcn@gmail.com>
2025-06-13 16:05:16 +0200

    RespuestaScreen Responsive
    oh yeah!

--- src/components/RespuestaScreen.jsx
+++ src/components/RespuestaScreen.jsx
@@ -1,9 +1,20 @@
+import Bola from "../assets/images/Bola_manos_sin.png";
+import "../StyleSheets/RespuestasScreen.css";
+
 function RespuestaScreen({ pregunta, respuesta, volver }) {
   return (
-    <div className="text-center space-y-6">
-      <h2 className="text-2xl font-bold"> Tu pregunta: </h2>
-      <p className="italic">{pregunta}</p>
-      <div className="text-3xl animate-pulse"> {respuesta}</div>
+    <div className="flex flex-col items-center justify-evenly px-4 container max-w-5xl gap-y-1">
+      <h1 className="h1Respuesta"> El destino ha hablado </h1>
+      <h2 className="Pregunta">{pregunta}</h2>
+
+      <div className="relative w-4/5 max-w-md">
+        <img src={Bola} alt="Respuesta" className="RespuestaBola w-full h-auto " />
+        <p className="RespuestaTexto absolute inset-0 flex items-center justify-center md:text-4xl animate-pulse">
+          {" "}
+          {respuesta}
+        </p>
+      </div>
+
       <button onClick={() => volver("home")} className="botonInicio">
         Inicio
       </button>

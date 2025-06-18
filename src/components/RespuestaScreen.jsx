7fb94ae2f677d8753adf22101a649b69a312bbc7
JessCardenas <jesscardenasbcn@gmail.com>
2025-06-18 16:34:04 +0200

    update respuesta-screen

--- src/components/RespuestaScreen.jsx
+++ src/components/RespuestaScreen.jsx
@@ -3,7 +3,7 @@ import "../StyleSheets/RespuestasScreen.css";
 
 function RespuestaScreen({ pregunta, respuesta, volver }) {
   return (
-    <div className="respuesta-screen flex flex-col items-center justify-center px-4 container max-w-5xl -translate-y-10 md:-translate-y-0">
+    <div className="respuesta-screen flex flex-col items-center justify-center px-4 container max-w-5xl min-h-screen -translate-y-10 md:-translate-y-0">
       <h1 className="h1Respuesta"> El destino ha hablado </h1>
       <h2 className="Pregunta">{pregunta}</h2>
 
